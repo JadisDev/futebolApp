5de489b0a1278dcaed5d1dd3a574b49a0f4b3585
Jadis Jale <jadisjale@hotmail.com>
2020-12-31 18:39:46 -0300

    começado integraçao com login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,55 @@ import { View, Text, TouchableOpacity, Alert, Image } from 'react-native';
 import Welcome from './Welcome'
 import styles from '../styles/index'
 import { TextInput } from 'react-native-gesture-handler';
+import axios from 'axios';
+import consts from '../const';
 
 class Login extends Component {
 
+    state = {
+        login: 'jsj',
+        password: '112233',
+        name_group: 'EBD'
+    }
+    consumeApiLogin = () => {
+
+        axios.post(`${consts.API_URL}/sessions`, this.state)
+        .then(resp => {
+            console.log(resp.data.user.name);
+            console.log(resp.data.group.name_group);
+            console.log(resp.data.user.admin);
+            console.log(resp.data.token);
+        })
+        .catch(e => {
+            console.log(e.message);
+        });
+
+    }
+
     login = () => {
-        this.props.navigation.navigate("Home");
+        const {login, password, name_group} = this.state;
+        if (login === '') {
+            Alert.alert('Informe o login para continuar');
+            return;
+        }
+        if (login.length < 3) {
+            Alert.alert('O login deve mais de 2 caracteres');
+            return;
+        }
+        if (password === '') {
+            Alert.alert('Informe sua senha');
+            return;
+        }
+        if (name_group === '') {
+            Alert.alert('Informe grupo para entrar');
+            return;
+        }
+        if (name_group.length < 3) {
+            Alert.alert('O grupo deve mais de 2 caracteres');
+            return;
+        }
+
+        this.consumeApiLogin();
     }
 
     signUp = () => {
@@ -15,6 +59,7 @@ class Login extends Component {
     }
 
     render() {
+
         return (
             <View style={styles.container}>
 
@@ -31,18 +76,22 @@ class Login extends Component {
                 <TextInput
                     style={styles.input}
                     placeholder="Digite seu login"
+                    onChangeText={(value) => this.setState({ login: value })}
+                    value={this.state.login}
                 />
 
                 <TextInput
                     style={styles.input}
                     secureTextEntry={true}
                     placeholder="Digite sua senha"
+                    onChangeText={(value) => this.setState({ password: value })}
+                    value={this.state.password}
                 />
 
                 <TextInput
                     style={styles.input}
-                    secureTextEntry={true}
-                    placeholder="Digite nome grupo"
+                    onChangeText={(value) => this.setState({ name_group: value })}
+                    value={this.state.name_group}
                 />
 
                 <TouchableOpacity
---
+++ src/const.js
@@ -0,0 +1,4 @@
+export default {
+    // API_URL: 'http://localhost:3333/api',
+    API_URL: 'http://192.168.10.18:3333/api'
+}
